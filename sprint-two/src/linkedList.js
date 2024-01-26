da3fe29d5e94cd8b997ef18a507fffebd3ad59b5
Richard Li <richardli1745@gmail.com>
2024-01-25 17:36:07 -0800

    pass first two data structures:Linked List and Tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,64 @@
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    //debugger;
+    //if empty
+    if (this.head === null && this.tail === null) {
+      //create new node to initalize Linkedlist
+      var newNode = Node(value);
+      this.head = newNode;
+      this.tail = newNode;
+      //if not empty aka single node
+    } else if (this.head === this.tail) {
+      //create node for new tail with value passed in
+      var newTail = Node(value);
+      this.tail = newTail;
+      this.head.next = newTail;
+      //if more than one node
+    } else {
+      var newTail = Node(value);
+      this.tail.next = newTail;
+      this.tail = newTail;
+    }
   };
 
   list.removeHead = function() {
+    //declare variable to store this.head.next
+    var remove = this.head.value;
+    var newHead = this.head.next;
+    //delete this.head
+    delete this.head;
+    //reassign this.head to newHead
+    this.head = newHead;
+    return remove;
+
+
   };
 
   list.contains = function(target) {
+
+    var search = this.head;
+    var isDone = false;
+    while (isDone !== true) {
+      //check if value is = to target
+      //debugger;
+      if (search === this.tail) {
+        if (search.value === target) {
+          return true;
+        } else {
+          isDone = true;
+        }
+      }
+      if (search.value === target) {
+        return true;
+      }
+      search = search.next;
+    }
+    return false;
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +13,47 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+
+  // inputs: any value
+  // outputs: no output, but adds to children array
+  // constraints:
+  // edge cases:
+
+  // create a variable that is a tree representing the new value
+  var newChild = Tree(value);
+
+  // add that variable to the children of the tree
+  this.children.push(newChild);
+
 };
 
 treeMethods.contains = function(target) {
+  //debugger;
+  // inputs: a target value that we compare to each node
+  // outputs: a boolean saying whether or not the target is found
+  // constraints:
+  // edge cases:
+
+  //iterate through each child
+  var isTarget = false;
+  for (var i = 0; i < this.children.length; i++) {
+    //declare child variable
+    var child = this.children[i];
+    console.log(child);
+    //check if target value is matching
+    if (child.value === target) {
+      //return true if so
+      isTarget = true;
+      return isTarget;
+      // break;
+    } else {
+      //else call on contains with child
+      isTarget = isTarget || child.contains(target);
+    }
+  }
+  return isTarget;
+
+  //return false if break out of loop
 };
 
 
